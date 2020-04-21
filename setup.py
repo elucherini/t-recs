a113ece0994bdee08563c8bf84ae53e9971082e6
Elena Lucherini <elucherini@princeton.edu>
2020-04-21 11:59:31 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='rec',
+        version='0.1',
+        url='https://github.com/elucherini/algo-segregation',
+        license='GPL-3.0',
+        author='Elena Lucherini',
+        author_email='elucherini@cs.princeton.edu',
+        description='Framework for simulating recommendation systems and more.',
+        long_description=readme(),
+        packages=find_packages(),
+        install_requires=[
+            'numpy>=1.17.0',
+            'scipy>=1.4.1',
+            'pandas>=0.25.0',
+            'networkx>=2.4',
+        ],
+        zip_safe=False)
