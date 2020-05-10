c3a99b6c1401cdc317f32ffe1f7b6c735802d959
Elena Lucherini <elucherini@princeton.edu>
2020-05-10 10:42:42 -0400

    Allow generator to accept any kind of bitgenerators

--- rec/distribution.py
+++ rec/distribution.py
@@ -3,5 +3,7 @@ import numpy as np
 """Wrapper around numpy.random.Generator
 """
 class Generator(np.random.Generator):
-    def __init__(self, *args, **kwargs):
-        np.random.Generator.__init__(self, np.random.PCG64(), *args, **kwargs)
+    def __init__(self, bitgenerator=None):
+        if bitgenerator is None:
+            bitgenerator = np.random.PCG64()
+        np.random.Generator.__init__(self, bitgenerator)
