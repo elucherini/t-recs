e16365613a755dd784465cbaeb78319938d316ca
Elena Lucherini <elucherini@princeton.edu>
2020-05-28 11:19:56 -0400

    Resurrecting PopularityRecommender from the dead

--- rec/models/__init__.py
+++ rec/models/__init__.py
@@ -2,3 +2,4 @@ from .recommender import BaseRecommender
 from .bass import BassModel
 from .content import ContentFiltering
 from .social import SocialFiltering
+from .popularity import PopularityRecommender
