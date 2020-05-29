e53310de34824ed06e84061b469027c3b4a2edcf
Elena Lucherini <elucherini@princeton.edu>
2020-05-29 18:40:22 -0400

    Add docstrings to metrics

--- rec/metrics/measurement.py
+++ rec/metrics/measurement.py
@@ -4,6 +4,24 @@ from rec.components import BaseObservable
 import numpy as np
 
 class Measurement(BaseObservable, VerboseMode, ABC):
+    """
+    Abstract observable class to store measurements.
+
+    Parameters
+    -----------
+
+        verbose: bool (optional, default: False)
+            If True, enables verbose mode. Disabled by default.
+
+        init_value: array_like or None or int or float (optional, default: None)
+            The value of the metric before the start of the simulation.
+
+    Attributes
+    -----------
+
+        measurement_history: list
+            List of measurements. A new element is added at each timestep.
+    """
     def __init__(self, verbose=False, init_value=None):
         VerboseMode.__init__(self, __name__.upper(), verbose)
         self.measurement_history = list()
@@ -12,9 +30,29 @@ class Measurement(BaseObservable, VerboseMode, ABC):
         self.measurement_history.append(init_value)
 
     def get_measurement(self):
+        """
+        Returns measurements. See :func:`~components.base_components.BaseObservable.get_observable` for more details.
+
+        Returns
+        --------
+            Measurements: dict
+        """
         return self.get_observable(data=self.measurement_history)
 
     def observe(self, observation, copy=True):
+        """
+        Stores measurements. It can be called by implementations to ensure consistency when storing different measurements.
+
+        Parameters
+        -----------
+
+        observation: array_like or int or float or None
+            Element that will be stored
+
+        copy: bool (optional, default: True)
+            If True, the function stores a copy of observation. Useful for :obj:`numpy.ndarray`.
+
+        """
         if copy:
             to_append = np.copy(observation)
         else:
@@ -26,26 +64,58 @@ class Measurement(BaseObservable, VerboseMode, ABC):
         pass
 
     def get_timesteps(self):
+        """
+        Returns the number of measurements stored (which is equivalent to the number of timesteps that the system has been measuring).
+
+        Returns
+        --------
+
+            Length of measurement_history: int
+        """
         return len(self.measurement_history)
 
 
 class InteractionMeasurement(Measurement):
+    """
+    Keeps track of the interactions between users and items.
+
+    Specifically, at each timestep, it stores a histogram of length `|I|`, where element `i` is the number of interactions received by item `i`.
+
+    Parameters
+    -----------
+
+        verbose: bool (optional, default: False)
+            If True, enables verbose mode. Disabled by default.
+
+    Attributes
+    -----------
+        Inherited by Measurement: :class:`.Measurement`
+
+        name: str
+            Name of the measurement component.
+    """
     def __init__(self, verbose=False):
         self.name = 'interaction_histogram'
         Measurement.__init__(self, verbose, init_value=None)
 
     def _generate_interaction_histogram(self, interactions, num_users, num_items):
-        ''' Internal function that returns a histogram of the number
-            of interactions per item at the given timestep.
+        """
+        Generates a histogram of the number of interactions per item at the given timestep.
 
-            Args:
-                interactions (:obj:`numpy.array`): array of user interactions.
-                num_users (int): number of users in the system
-                num_items (int): number of items in the system
+        Parameters
+        -----------
 
-            Returns: :obj:`numpy.array` histogram of the number of interactions
-                aggregated by items at the given timestep.
-        '''
+            interactions (:obj:`numpy.ndarray`): array of user interactions.
+
+            num_users (int): number of users in the system
+
+            num_items (int): number of items in the system
+
+        Returns
+        ---------
+            :obj:`numpy.array` histogram of the number of interactions
+            aggregated by items at the given timestep.
+        """
         histogram = np.zeros(num_items)
         np.add.at(histogram, interactions, 1)
         # Check that there's one interaction per user
@@ -53,6 +123,22 @@ class InteractionMeasurement(Measurement):
         return histogram
 
     def measure(self, step, interactions, recommender):
+        """
+            Measures and stores a histogram of the number of interactions per item at the given timestep.
+
+            Parameters
+            ------------
+
+                step: int
+                    Current time step
+
+                interactions: :obj:`numpy.array`
+                    Non-aggregated array of interactions -- that is, an array of length `|U|` s.t. element `u` is the index of the item with which user `u` interacted.
+
+                recommender: :class:`~models.recommender.BaseRecommender`
+                    Model that inherits from :class:`~models.recommender.BaseRecommender`.
+        """
+
         histogram = self._generate_interaction_histogram(interactions,
                                                          recommender.num_users,
                                                          recommender.num_items)
@@ -61,6 +147,29 @@ class InteractionMeasurement(Measurement):
 
 
 class HomogeneityMeasurement(InteractionMeasurement):
+    """
+    Measures the homogeneity of the interactions between users and items.
+
+    Specifically, at each timestep, it measures whether interactions are spread among many items or only a few items.
+
+    This class inherits from :class:`.InteractionMeasurement`.
+
+    Parameters
+    -----------
+
+        verbose: bool (optional, default: False)
+            If True, enables verbose mode. Disabled by default.
+
+    Attributes
+    -----------
+        Inherited by InteractionMeasurement: :class:`.InteractionMeasurement`
+
+        name: str
+            Name of the measurement component.
+
+        _old_histogram: None, list, array_like
+            A copy of the histogram at the previous timestep.
+    """
     def __init__(self, verbose=False):
         self.histogram = None
         self._old_histogram = None
@@ -68,21 +177,21 @@ class HomogeneityMeasurement(InteractionMeasurement):
         Measurement.__init__(self, verbose, init_value=None)
 
     def measure(self, step, interactions, recommender):
-        '''
-            Measures the homogeneity of user interactions
-            (i.e., whether interactions are spread among many items or only a
-            few items).
+        """
+            Measures the homogeneity of user interactions -- that is, whether interactions are spread among many items or only a few items.
 
-            Args:
-                step (int): current time step
-                interactions (:obj:`numpy.array`): non-aggregated array of interactions (i.e.,
-                     array of length |U| s.t. element u is the index of the
-                     item user u interacted with)
-                num_users (int): number of users in the system
-                num_items (int): number of items in the system
+            Parameters
+            ------------
 
-            Returns: :obj:`numpy.array` with homogeneity.
-        '''
+                step: int
+                    Current time step
+
+                interactions: :obj:`numpy.array`
+                    Non-aggregated array of interactions -- that is, an array of length `|U|` s.t. element `u` is the index of the item with which user `u` interacted.
+
+                recommender: :class:`~models.recommender.BaseRecommender`
+                    Model that inherits from :class:`~models.recommender.BaseRecommender`.
+        """
         assert(interactions.size == recommender.num_users)
         histogram = self._generate_interaction_histogram(interactions,
                                                          recommender.num_users,
@@ -97,26 +206,83 @@ class HomogeneityMeasurement(InteractionMeasurement):
 
 
 class MSEMeasurement(Measurement):
+    """
+    Measures the mean squared error (MSE) between real and predicted user scores.
+
+    It can be used to evaluate how accurate the model predictions are.
+
+    This class inherits from :class:`.Measurement`.
+
+    Parameters
+    -----------
+
+        verbose: bool (optional, default: False)
+            If True, enables verbose mode. Disabled by default.
+
+    Attributes
+    -----------
+        Inherited by Measurement: :class:`.Measurement`
+
+        name: str
+            Name of the measurement component.
+    """
     def __init__(self, verbose=False):
         self.name = 'mse'
         Measurement.__init__(self, verbose, init_value=None)
 
     def measure(self, step, interactions, recommender):
-        """ Measures and records the mean
-            squared error between the user preferences predicted
-            by the system and the users' actual preferences.
+        """
+        Measures and records the mean squared error between the user preferences predicted by the system and the users' actual preferences.
 
-            Args:
-                predicted (:obj:`numpy.array`): user preferences
-                    predicted by the system
-                actual (:obj:`numpy.array`): actual user preferences,
-                    unknown to the system.
+        Parameters
+        ------------
+
+            step: int
+                Current time step
+
+            interactions: :obj:`numpy.array`
+                Non-aggregated array of interactions -- that is, an array of length `|U|` s.t. element `u` is the index of the item with which user `u` interacted.
+
+            recommender: :class:`~models.recommender.BaseRecommender`
+                Model that inherits from :class:`~models.recommender.BaseRecommender`.
         """
         self.observe(((recommender.predicted_scores -
             recommender.actual_users.actual_user_scores)**2).mean(), copy=False)
 
 
 class DiffusionTreeMeasurement(Measurement):
+    """
+    Class that implements an information diffusion tree. The current implementation assumes that agents using this class (i.e., a model) implement an :attr:`~models.bass.BassModel.infection_state` matrix that denotes the initial state of information.
+
+    In this implementation, the nodes represent users and are labeled with the user indices. A branch between nodes `u` and `v` indicates that user `u` passed information onto user `v` -- that is, `u` "infected" `v`.
+
+    Trees are implemented using the `Networkx library`_. Please refer to Networkx's `documentation`_ for more details.
+
+    .. _Networkx library: http://networkx.github.io
+    .. _documentation: https://networkx.github.io/documentation/stable/
+
+    Parameters
+    -----------
+
+        infection_state: array_like
+            The initial "infection state"
+
+        verbose: bool (optional, default: False)
+            If True, enables verbose mode. Disabled by default.
+
+    Attributes
+    -----------
+        Inherited by Measurement: :class:`.Measurement`
+
+        name: str
+            Name of the metric that is recorded at each time step. Note that, in this case, the metric stored in :attr:`~.Measurement.measurement_history` is actually the **number of infected users**. The diffusion tree itself is kept in the :attr:`.diffusion_tree` data structure.
+
+        diffusion_tree: :obj:`networkx.Graph`
+            Diffusion tree.
+
+        _old_infection_state: array_like
+            Infection state at the previous timestep.
+    """
     def __init__(self, infection_state, verbose=False):
         import networkx as nx
         self.name = 'num_infected'
@@ -161,22 +327,59 @@ class DiffusionTreeMeasurement(Measurement):
         return new_infected_users.shape[0]
 
     def measure(self, step, interactions, recommender):
+        """
+        Updates tree with new infections and stores information about new infections. In :attr:`~.Measurement.measurement_history`, it stores the total number of infected users in the system -- that is, the number of nodes in the tree.
+
+        Parameters
+        ------------
+
+            step: int
+                Current time step
+
+            interactions: :obj:`numpy.array`
+                Non-aggregated array of interactions -- that is, an array of length `|U|` s.t. element `u` is the index of the item with which user `u` interacted.
+
+            recommender: :class:`~models.recommender.BaseRecommender`
+                Model that inherits from :class:`~models.recommender.BaseRecommender`.
+        """
         num_new_infections = self._manage_new_infections(recommender.user_profiles,
             recommender.infection_state)
         self.observe(self.diffusion_tree.number_of_nodes(), copy=False)
         self._old_infection_state = np.copy(recommender.infection_state)
 
     def draw_tree(self):
+        """
+        Plots the tree using the Networkx library API.
+        """
         import matplotlib.pyplot as plt
         import networkx as nx
         nx.draw(self.diffusion_tree, with_labels=True)
 
 
 class StructuralVirality(DiffusionTreeMeasurement):
+    """
+    This class extends :class:`DiffusionTreeMeasurement` with the concept of structural virality developed by Goel, Anderson, Hofman, and Watts in `The Structural Virality of Online Diffusion`_. It is used in :class:`~models.bass.BassModel`.
+
+    .. _The Structural Virality of Online Diffusion: https://5harad.com/papers/twiral.pdf
+
+    Parameters
+    ----------
+
+        infection_state: array_like
+            The initial "infection state" (see :class:`DiffusionTreeMeasurement`).
+
+    """
     def __init__(self, infection_state, verbose=False):
         DiffusionTreeMeasurement.__init__(self, infection_state, verbose)
 
     def get_structural_virality(self):
+        """
+        Returns a measure of structural virality.
+
+        Returns
+        --------
+            Structural virality: float
+        """
         import networkx as nx
         from networkx.algorithms.wiener import wiener_index
         n = self.diffusion_tree.number_of_nodes()
