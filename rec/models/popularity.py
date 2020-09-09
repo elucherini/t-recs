c7931b0439b4abed9d224be4093cac3a1c89f900
Matthew Sun <matthewkol186@gmail.com>
2020-09-09 14:32:57 -0700

    Clean and reformat files in rec/models (#53)
    * Clean up comments and reformat code
    
    * Reformat and clean models/content.py
    
    * Reformat rec/models/social.py
    
    * Reformat recommender code
    
    * Reformat popularity model file

--- rec/models/popularity.py
+++ rec/models/popularity.py
@@ -1,17 +1,32 @@
 import numpy as np
 from rec.models import BaseRecommender
 from rec.metrics import MSEMeasurement
-from rec.utils import get_first_valid, is_array_valid_or_none, is_equal_dim_or_none, all_none, is_valid_or_none
+from rec.utils import (
+    get_first_valid,
+    is_array_valid_or_none,
+    is_equal_dim_or_none,
+    all_none,
+    is_valid_or_none,
+)
+
 
 class PopularityRecommender(BaseRecommender):
     """
     A customizable popularity recommendation system.
 
-    With the popularity recommender system, users are presented items that are popular in the system. The popularity of an item is measured by the number of times users interacted with that item in the past. In this implementation, items do not expire and, therefore, the system does not base its choice on how recent the items are.
+    With the popularity recommender system, users are presented items that are 
+    popular in the system. The popularity of an item is measured by the number
+    of times users interacted with that item in the past. In this
+    implementation, items do not expire and, therefore, the system does not base
+    its choice on how recent the items are.
 
-    Item attributes are represented by a `1x|I|` array, where `|I|` is the number of items in the system. This array stores the number of user interactions for each item.
+    Item attributes are represented by a `1x|I|` array, where `|I|` is the
+    number of items in the system. This array stores the number of user
+    interactions for each item.
 
-    User profiles are represented by a `|U|x1` matrix, where `|U|` is the number of users in the system. All elements of this matrix are equal to 1, as the predictions of the system are solely based on the item attributes.
+    User profiles are represented by a `|U|x1` matrix, where `|U|` is the number
+    of users in the system. All elements of this matrix are equal to 1, as the
+    predictions of the system are solely based on the item attributes.
 
     Parameters
     -----------
@@ -23,13 +38,17 @@ class PopularityRecommender(BaseRecommender):
             The number of items `|I|` in the system.
 
         item_representation: :obj:`numpy.ndarray` or None (optional, default: None)
-            A `|A|x|I|` matrix representing the similarity between each item and attribute. If this is not None, num_items is ignored.
+            A `|A|x|I|` matrix representing the similarity between each item
+            and attribute. If this is not None, num_items is ignored.
 
         user_representation: :obj:`numpy.ndarray` or None (optional, default: None)
-            A `|U|x|A|` matrix representing the similarity between each item and attribute, as interpreted by the system. If this is not None, num_users is ignored.
+            A `|U|x|A|` matrix representing the similarity between each item
+            and attribute, as interpreted by the system. If this is not None, num_users is ignored.
 
         actual_user_representation: :obj:`numpy.ndarray` or None (optional, default: None)
-            A `|U|x|I|` matrix representing the real user scores. This matrix is **not** used for recommendations. This is only kept for measurements and the system is unaware of it.
+            A `|U|x|I|` matrix representing the real user scores. This matrix is
+            **not** used for recommendations. This is only kept for measurements
+            and the system is unaware of it.
 
         verbose: bool (optional, default: False)
             If True, enables verbose mode. Disabled by default.
@@ -46,7 +65,8 @@ class PopularityRecommender(BaseRecommender):
 
     Examples
     ---------
-        PopularityRecommender can be instantiated with no arguments -- in which case, it will be initialized with the default parameters.
+        PopularityRecommender can be instantiated with no arguments -- in which
+        case, it will be initialized with the default parameters.
 
         >>> pr = PopularityRecommender()
         >>> pr.user_profiles.shape
@@ -63,7 +83,10 @@ class PopularityRecommender(BaseRecommender):
         >>> cf.item_attributes.shape
         (1, 5000)
 
-        Or by generating representations for items (user representation can also be defined, but they should always be set to all ones). In the example below, items are uniformly distributed and have had between 0 and 10 interactions each.
+        Or by generating representations for items (user representation can
+        also be defined, but they should always be set to all ones). In the
+        example below, items are uniformly distributed and have had between 0
+        and 10 interactions each.
 
             >>> item_representation = np.random.randint(11, size=(1, 200))
             >>> pr = PopularityRecommender(item_representation=item_representation)
@@ -72,8 +95,9 @@ class PopularityRecommender(BaseRecommender):
             >>> cf.user_profiles.shape
             (100, 1)
 
-        Note that user and item representations have the precedence over the number of
-        users and the number of items specified at initialization. For example:
+        Note that user and item representations have the precedence over the
+        number of users and the number of items specified at initialization.
+        For example:
 
         >>> user_representation = np.ones((3000, 1))
         >>> pr = PopularityRecommender(num_users=50, user_representation=user_representation)
@@ -81,59 +105,78 @@ class PopularityRecommender(BaseRecommender):
         (3000, 1) # <-- 30000 users. num_users was ignored because user_representation was specified.
 
     """
-    def __init__(self, num_users=100, num_items=1250,
-                 item_representation=None, user_representation=None,
-                 actual_user_representation=None,seed=None,
-                verbose=False, num_items_per_iter=10):
+
+    def __init__(
+        self,
+        num_users=100,
+        num_items=1250,
+        item_representation=None,
+        user_representation=None,
+        actual_user_representation=None,
+        seed=None,
+        verbose=False,
+        num_items_per_iter=10,
+    ):
 
         if all_none(item_representation, num_items):
-                raise ValueError("num_items and item_representation can't be all None")
+            raise ValueError("num_items and item_representation can't be all None")
         if all_none(user_representation, num_users):
-                raise ValueError("num_users and user_representation can't be all None")
+            raise ValueError("num_users and user_representation can't be all None")
 
         if not is_array_valid_or_none(item_representation, ndim=2):
             raise ValueError("item_representation is not valid")
         if not is_array_valid_or_none(user_representation, ndim=2):
             raise ValueError("item_representation is not valid")
 
-        num_items = get_first_valid(getattr(item_representation, 'shape',
-                                            [None, None])[1],
-                                    num_items)
+        num_items = get_first_valid(
+            getattr(item_representation, "shape", [None, None])[1], num_items
+        )
 
-        num_users = get_first_valid(getattr(user_representation, 'shape', [None])[0],
-                                    num_users)
+        num_users = get_first_valid(
+            getattr(user_representation, "shape", [None])[0], num_users
+        )
 
         if item_representation is None:
             item_representation = np.zeros((1, num_items), dtype=int)
         if user_representation is None:
             user_representation = np.ones((num_users, 1), dtype=int)
 
-
-        if not is_equal_dim_or_none(getattr(user_representation,
-                                            'shape', [None, None])[1],
-                                    getattr(item_representation,
-                                            'shape', [None])[0]):
-            raise ValueError("user_representation.shape[1] should be the same as " + \
-                             "item_representation.shape[0]")
-        if not is_equal_dim_or_none(getattr(user_representation,
-                                            'shape', [None])[0],
-                                    num_users):
-            raise ValueError("user_representation.shape[0] should be the same as " + \
-                             "num_users")
-        if not is_equal_dim_or_none(getattr(item_representation,
-                                            'shape', [None, None])[1],
-                                    num_items):
-            raise ValueError("item_representation.shape[1] should be the same as " + \
-                             "num_items")
+        if not is_equal_dim_or_none(
+            getattr(user_representation, "shape", [None, None])[1],
+            getattr(item_representation, "shape", [None])[0],
+        ):
+            raise ValueError(
+                "user_representation.shape[1] should be the same as "
+                + "item_representation.shape[0]"
+            )
+        if not is_equal_dim_or_none(
+            getattr(user_representation, "shape", [None])[0], num_users
+        ):
+            raise ValueError(
+                "user_representation.shape[0] should be the same as " + "num_users"
+            )
+        if not is_equal_dim_or_none(
+            getattr(item_representation, "shape", [None, None])[1], num_items
+        ):
+            raise ValueError(
+                "item_representation.shape[1] should be the same as " + "num_items"
+            )
 
         measurements = [MSEMeasurement()]
 
-        super().__init__(user_representation, item_representation,
-                         actual_user_representation, num_users, num_items,
-                         num_items_per_iter,
-                         measurements=measurements, verbose=verbose, seed=seed)
+        super().__init__(
+            user_representation,
+            item_representation,
+            actual_user_representation,
+            num_users,
+            num_items,
+            num_items_per_iter,
+            measurements=measurements,
+            verbose=verbose,
+            seed=seed,
+        )
 
     def _update_user_profiles(self, interactions):
         histogram = np.zeros(self.num_items)
         np.add.at(histogram, interactions, 1)
-        self.item_attributes[:,:] = np.add(self.item_attributes, histogram)
+        self.item_attributes[:, :] = np.add(self.item_attributes, histogram)
