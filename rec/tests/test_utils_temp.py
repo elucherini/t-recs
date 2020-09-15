508c38d8512c1ba1b68df0c7a0edc32e1591bab8
Matthew Sun <matthewkol186@gmail.com>
2020-09-15 14:26:56 -0400

    Test normalize_matrix and fix error with passing in a vector

---
+++ rec/tests/test_utils_temp.py
@@ -0,0 +1,40 @@
+import numpy as np
+import test_utils
+from rec.utils import (
+    normalize_matrix
+)
+
+class TestUtils:
+    def test_normalize_matrix(self):
+        # matrix that already has norm 1 columns/rows
+        mat_1 = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+        test_utils.assert_equal_arrays(mat_1, normalize_matrix(mat_1))
+        test_utils.assert_equal_arrays(mat_1, normalize_matrix(mat_1, axis=0))
+        
+        # matrix with norm 2 columns/rows
+        mat_2 = np.array([
+            [2, 0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 0, 2, 0],
+            [0, 0, 0, 2]
+        ])
+        test_utils.assert_equal_arrays(mat_1, normalize_matrix(mat_2))
+        test_utils.assert_equal_arrays(mat_1, normalize_matrix(mat_2, axis=0))
+
+        # check norm of all rows equals 1 after normalization
+        mat_3 = np.arange(16).reshape((4,4))
+        normalized = normalize_matrix(mat_3, axis=1)
+        assert (np.linalg.norm(normalized, axis=1) == 1).all()
+
+    def test_normalize_vector(self):
+        vec = np.array([3, 4])
+        unit_vec = normalize_matrix(vec)
+        correct_unit_vec = np.array(
+            [[3/5, 4/5]
+        ])
+        test_utils.assert_equal_arrays(unit_vec, correct_unit_vec)
