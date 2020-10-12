1941c5ebc33e81adccbad62cbb0903c7006db346
Matthew Sun <matthewkol186@gmail.com>
2020-10-11 21:27:12 -0400

    Write small docstring:

--- rec/components/__init__.py
+++ rec/components/__init__.py
@@ -1,3 +1,4 @@
+"""Components shared across multiple types of models (e.g., users and items)"""
 from .items import Items
 from .socialgraph import BinarySocialGraph
 from .users import Users, PredictedUserProfiles, PredictedScores
@@ -5,6 +6,7 @@ from .base_components import (
     Component,
     BaseComponent,
     BaseObservable,
-    BaseObserver,
     FromNdArray,
+    register_observables,
+    unregister_observables
 )
