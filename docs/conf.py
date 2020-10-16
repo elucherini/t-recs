11f96b2f4a5797b015284b79cab7b5e9735e51ff
Matthew Sun <matthewkol186@gmail.com>
2020-10-16 12:26:53 -0400

    Modify configuration so it uses the correct folder

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath("../rec/"))
+sys.path.insert(0, os.path.abspath("../trecs/"))
 sys.setrecursionlimit(1500)
 
 # -- Project information -----------------------------------------------------
