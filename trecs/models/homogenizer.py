bd0cfa125a07673fe6bcc96a96f7be33a6577e80
amywinecoff <aawinecoff@gmail.com>
2021-02-08 11:08:55 -0500

    testing and visualizing MF in addition to CF in the notebook

--- trecs/models/homogenizer.py
+++ trecs/models/homogenizer.py
@@ -161,21 +161,6 @@ class Homogenizer(BaseRecommender):
                  num_items_per_iter,
                  **kwargs
         )
-        print ('initializing Child instance')
-        # pylint: disable=duplicate-code
-        # num_users, num_items, num_attributes = validate_user_item_inputs(
-        #     num_users,
-        #     num_items,
-        #     user_representation,
-        #     item_representation,
-        #     actual_user_representation,
-        #     actual_item_representation,
-        #     100,
-        #     1250,
-        #     1000,
-        #     num_attributes=num_attributes,
-        # )
-
 
         assert 0 < homogenization_increment < 1
 
@@ -183,22 +168,6 @@ class Homogenizer(BaseRecommender):
         # and item attributes
         gen = Generator(seed=seed)
 
-        # if user_representation is None:
-        #     #user_representation = gen.normal(size=(num_users, num_attributes))
-        #     user_representation = np.zeros((num_users, num_attributes))
-        # if item_representation is None:
-        #     item_representation = gen.binomial(
-        #         n=1, p=0.5, size=(num_attributes, num_items)
-        #     )
-
-        #gen = Generator(seed=seed)
-        #attribute that retains a static representation where all users have the same values
-        # self.homogenized_users_hat = np.tile(gen.normal(size=(1, num_attributes)) , (num_users,1))
-        # #attribute that retains the indivdiual users' preference representation in the algorithn
-        # self.individual_users_hat = user_representation
-        # #proportion of users_hat that will come from the homogenized representation
-        # self.homogenization_proportion = 0
-        # self.homogenization_increment = homogenization_increment
 
         self.homogenized_users_hat = np.tile(gen.normal(size=(1, num_attributes)) , (self.num_users,1))
         #attribute that retains the indivdiual users' preference representation in the algorithn
@@ -206,36 +175,6 @@ class Homogenizer(BaseRecommender):
         #proportion of users_hat that will come from the homogenized representation
         self.homogenization_proportion = 0
         self.homogenization_increment = homogenization_increment
-
-        # if the actual item representation is not specified, we assume
-        # that the recommender system's beliefs about the item attributes
-        # are the same as the "true" item attributes
-        #if actual_item_representation is None:
-        #    actual_item_representation = np.copy(item_representation)
-        #if actual_user_representation is None:
-        #    actual_user_representation=gen.normal(size=(num_users, num_attributes))
-
-
-
-
-        # Initialize recommender system
-        # BaseRecommender.__init__(
-        #     self,
-        #     user_representation,
-        #     item_representation,
-        #     actual_user_representation,
-        #     actual_item_representation,
-        #     num_users,
-        #     num_items,
-        #     num_items_per_iter,
-        #     probabilistic_recommendations=probabilistic_recommendations,
-        #     measurements=measurements,
-        #     verbose=verbose,
-        #     seed=seed,
-        #     **kwargs
-        # )
-
-
 
     def _update_internal_state(self, interactions):
         """
@@ -255,15 +194,12 @@ class Homogenizer(BaseRecommender):
                 the item that the user has interacted with.
 
         """
-        print("Homogenization proportion is {}".format(self.homogenization_proportion))
+        #print("Homogenization proportion is {}".format(self.homogenization_proportion))
         interactions_per_user = np.zeros((self.num_users, self.num_items))
         interactions_per_user[self.users.user_vector, interactions] = 1
         user_attributes = np.dot(interactions_per_user, self.items_hat.T)
-        #increment individual preferences the same way as content filtering
+
         self.individual_users_hat += user_attributes
-
-        #print("self.users_hat is {}".format(self.users_hat[0]))
-
 
         if self.homogenization_proportion < 1:
             self.users_hat = (self.homogenization_proportion * self.homogenized_users_hat) + ((1-self.homogenization_proportion) * self.individual_users_hat)
@@ -271,32 +207,7 @@ class Homogenizer(BaseRecommender):
         else:
             self.users_hat = self.homogenized_users_hat
 
-    # def run(
-    #     self,
-    #     timesteps=50,
-    #     startup=False,
-    #     train_between_steps=False,
-    #     random_items_per_iter=0,
-    #     vary_random_items_per_iter=False,
-    #     repeated_items=True,
-    #     no_new_items=False,
-    # ):
-    #     """
-    #     Just a simple wrapper so that by default, the RS does not implement the homogenization during startup
-    #     """
-    #     if startup:
-    #         #reset the homogenization proportion to 0 if we are in startup mode
-    #         self.homogenization_proportion = 0
-    #
-    #     super().run(
-    #         timesteps,
-    #         startup,
-    #         train_between_steps,
-    #         random_items_per_iter,
-    #         vary_random_items_per_iter,
-    #         repeated_items,
-    #         no_new_items=no_new_items,
-    #     )
+
 
     def process_new_items(self, new_items):
         """
@@ -311,22 +222,3 @@ class Homogenizer(BaseRecommender):
                 added into the system. Should be :math:`|A|\\times|I|`
         """
         self.items_hat = np.hstack([self.items_hat, new_items])
-
-
-# class Child(object):
-#     def __init__(self, baseclass):
-#         self.__class__ = type(self.__class__.__name__,
-#                               (baseclass, object),
-#                               dict(self.__class__.__dict__))
-#         super(self.__class__, self).__init__()
-#         print 'initializing Child instance'
-#         # continue with Child class' initialization...
-#
-# class SomeParentClass(object):
-#     def __init__(self):
-#         print 'initializing SomeParentClass instance'
-#     def hello(self):
-#         print 'in SomeParentClass.hello()'
-#
-# c = Child(SomeParentClass)
-# c.hello()
