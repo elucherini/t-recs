bb11725f3217fce8dd936bda53b16dbb0b04bf95
amywinecoff <aawinecoff@gmail.com>
2021-03-03 09:24:58 -0500

    started working lfd rec list generation into the model class

--- trecs/models/mflfd.py
+++ trecs/models/mflfd.py
@@ -46,28 +46,28 @@ class ImplicitMFLFD(ImplicitMF):
             **kwargs,
         )
 
-    def generate_recommendations(self, k=1, item_indices=None):
+    def generate_recommendations(self, k=1, top_n_limit=None, item_indices=None):
         """
-        Generate recommendations for each user.
+                Generate recommendations for each user.
 
-        Parameters
-        -----------
+                Parameters
+                -----------
 
-            k : int (optional, default: 1)
-                Number of items to recommend.
+                    k : int (optional, default: 1)
+                        Number of items to recommend.
 
-            item_indices : :obj:`numpy.ndarray` or None (optional, default: None)
-                A matrix containing the indices of the items each user has not yet
-                interacted with. It is used to ensure that the user is presented
-                with items they have not already interacted with. If `None`,
-                then the user may be recommended items that they have already
-                interacted with.
+                    item_indices : :obj:`numpy.ndarray` or None (optional, default: None)
+                        A matrix containing the indices of the items each user has not yet
+                        interacted with. It is used to ensure that the user is presented
+                        with items they have not already interacted with. If `None`,
+                        then the user may be recommended items that they have already
+                        interacted with.
 
-        Returns
-        ---------
-            Recommendations: :obj:`numpy.ndarray`
-        """
-        self.item_indices = item_indices
+                Returns
+                ---------
+                    Recommendations: :obj:`numpy.ndarray`
+                """
+
         if item_indices is not None:
             if item_indices.size < self.num_users:
                 raise ValueError(
@@ -78,46 +78,77 @@ class ImplicitMFLFD(ImplicitMF):
                 raise ValueError(
                     f"There are not enough items left to recommend {k} items to each user."
                 )
-        if k == 0:
-            return np.array([]).reshape((self.num_users, 0)).astype(int)
-        row = np.repeat(self.users.user_vector, item_indices.shape[1])
+            if k == 0:
+                return np.array([]).reshape((self.num_users, 0)).astype(int)
+
+        if not top_n_limit:
+            top_n_limit = self.items_hat.shape[1]
+
+        row = np.repeat(self.users.user_vector, self.item_indices.shape[1])
         row = row.reshape((self.num_users, -1))
-        s_filtered = self.predicted_scores[row, item_indices]
-        if self.probabilistic_recommendations:
-            permutation = s_filtered.argsort()
-            rec = item_indices[row, permutation]
-            # the recommended items will not be exactly determined by
-            # predicted score; instead, we will sample from the sorted list
-            # such that higher-preference items get more probability mass
-            num_items_unseen = rec.shape[1]  # number of items unseen per user
-            probabilities = np.logspace(0.0, num_items_unseen / 10.0, num=num_items_unseen, base=2)
-            probabilities = probabilities / probabilities.sum()
-            picks = np.random.choice(num_items_unseen, k, replace=False, p=probabilities)
-            return rec[:, picks]
-        else:
-            # scores are U x I; we can use argpartition to take the top k scores
-            negated_scores = -1 * s_filtered  # negate scores so indices go from highest to lowest
-            # break ties using a random score component
-            scores_tiebreak = np.zeros(
-                negated_scores.shape, dtype=[("score", "f8"), ("random", "f8")]
-            )
-            scores_tiebreak["score"] = negated_scores
-            scores_tiebreak["random"] = self.random_state.random(negated_scores.shape)
-            top_k = scores_tiebreak.argpartition(k - 1, order=["score", "random"])[:, :k]
-            # now we sort within the top k
-            row = np.repeat(self.users.user_vector, k).reshape((self.num_users, -1))
-            # again, indices should go from highest to lowest
-            sort_top_k = scores_tiebreak[row, top_k].argsort(order=["score", "random"])
-            rec = item_indices[
-                row, top_k[row, sort_top_k]
-            ]  # extract items such that rows go from highest scored to lowest-scored of top-k
-            if self.is_verbose():
-                self.log(f"Item indices:\n{str(item_indices)}")
-                self.log(
-                    f"Top-k items ordered by preference (high to low) for each user:\n{str(rec)}"
-                )
-            self.rec = rec
-            return rec
+        s_filtered = self.predicted_scores[row, self.item_indices]
+
+        negated_scores = -1 * s_filtered  # negate scores so indices go from highest to lowest
+        # break ties using a random score component
+        scores_tiebreak = np.zeros(
+            negated_scores.shape, dtype=[("score", "f8"), ("random", "f8")]
+        )
+        scores_tiebreak["score"] = negated_scores
+        scores_tiebreak["random"] = self.random_state.random(negated_scores.shape)
+        top_k = scores_tiebreak.argpartition(top_n_limit - 1, order=["score", "random"])[:, :top_n_limit]
+        # now we sort within the top k
+        row = np.repeat(self.users.user_vector, top_n_limit).reshape((self.num_users, -1))
+        # again, indices should go from highest to lowest
+        sort_top_k = scores_tiebreak[row, top_k].argsort(order=["score", "random"])
+        top_k_recs = self.item_indices[row, top_k[row, sort_top_k]]
+
+        # dims are attribute, items, users
+        top_k_att = mflfd.items_hat[:, top_k_recs[:]].swapaxes(1, 2)
+
+        rec = []
+        for idx, user in enumerate(mflfd.users_hat):
+
+            # make a copy so as not to modify the original array
+            user_item_feats = np.array(top_k_att[:, :, idx])
+
+            orig_user_item_feats = np.array(user_item_feats)
+            # user_item_feats_idx = [0]
+            user_max_idx = top_k_recs[idx, 0]
+            recs_idxs = [user_max_idx]
+
+            # hold the features of the recommended items
+            recs_features = self.items_hat[:, user_max_idx]
+
+            for rec in range(1, k):
+
+                if rec == 1:
+                    # for the second item, just use the first item values
+                    centroid = recs_features
+                else:
+                    centroid = np.nanmean(recs_features, axis=0)
+
+                centroid = centroid.reshape(1, -1)
+
+                # set all the previously chosen item features to the centroid, so they will not be selected again
+                # don't want to just remove rows because it will throw off the indexing
+                user_item_feats[:, 0:rec + 1] = centroid.T
+
+                d = pairwise_distances(X=centroid, Y=user_item_feats.T, metric='cityblock',
+                                       force_all_finite='allow_nan')
+
+                most_distant = np.argmax(d)
+
+                distances.append(d.max())
+
+                most_distant_feats = user_item_feats.T[most_distant]
+
+                # get the index of the most distant item in the top k recs
+                recs_idxs.append(top_k_recs[idx, most_distant])
+                recs_features = np.vstack((recs_features, user_item_feats[:, most_distant]))
+
+            rec.append(recs_idxs)
+
+        return rec
 
     # def latent_factors_diversification(self, top_n_limit=None):
     #
