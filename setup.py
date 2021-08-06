8bdcee04e499d8813a043ba80fbb4cf60db44523
Matthew Sun <matthewsun98@gmail.com>
2021-08-05 20:12:19 -0400

    Convert to setup.py to take advantage of subpackage structure

--- setup.py
+++ setup.py
@@ -1,3 +1,33 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup()
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+setup(
+    name="trecs",
+    version="0.2.1",
+    description="Framework for simulating sociotechnical systems.",
+    url="https://github.com/elucherini/t-recs",
+    license="MIT",
+    author="Eli Lucherini",
+    author_email="elucherini@cs.princeton.edu",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=readme(),
+    packages=["trecs", "trecs.models", "trecs.metrics", "trecs.components", "trecs.base", "trecs.random"],
+    install_requires=[
+        "numpy>=1.17.0",
+        "scipy>=1.4.1",
+        "networkx>=2.4",
+        "tqdm>=4.46.0",
+        "lenskit>=0.11.1",
+        "pandas==1.0.5",
+    ],
+    zip_safe=False,
+)
